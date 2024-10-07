5a52c1b0155b73d252e4bdc2a280e2565d3b720c
oleh <phd.chornyi@gmail.com>
2024-10-07 10:03:32 +0300

    2nd: screens

---
+++ lib/screens/buttons_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ButtonsScreen extends StatefulWidget {
+  const ButtonsScreen({super.key});
+
+  @override
+  State<ButtonsScreen> createState() => _ButtonsScreenState();
+}
+
+class _ButtonsScreenState extends State<ButtonsScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Buttons Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Choose what to do: increment or decrement',
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text('Increment')),
+            ElevatedButton(onPressed: () {}, child: const Text('Decrement')),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.arrow_back),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:providers_app/screens/buttons_screen.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'The current number is:',
+            ),
+            Text(
+              'Here goes the number',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => ButtonsScreen(),
+            ),
+          );
+        },
+        tooltip: 'Increment-Decrement',
+        child: const Icon(Icons.calculate),
+      ),
+    );
+  }
+}
